c681132ca7de89891f9d381cc0e1a639801e6538
sashakosolapov <sashakosolapov1998@yandex.ru>
2025-03-14 02:04:58 +0300

    Сверстан экран пользователя

---
+++ ImageFeed/Profiles/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Александр Косолапов on 13.03.2025.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+}
