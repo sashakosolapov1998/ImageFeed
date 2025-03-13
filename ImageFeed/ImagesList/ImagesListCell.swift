18c783288e9af6173a72d76b45c7223d4009ad5f
sashakosolapov <sashakosolapov1998@yandex.ru>
2025-03-13 18:46:30 +0300

    доработка button и label

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -7,17 +7,21 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
+    
+    // MARK: - Static properties
     static let reuseIdentifier = "ImagesListCell"
     
+    // MARK: - @IBOutlet properties
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var likeButton: UIButton!
     @IBOutlet var cellImage: UIImageView!
     
+    // MARK: - Override methods
     override func awakeFromNib() {
-            super.awakeFromNib()
-
-            likeButton.setImage(UIImage(named: "like_button_off"), for: .normal)
-            likeButton.setImage(UIImage(named: "like_button_on"), for: .selected)
-        }
+        super.awakeFromNib()
+        
+        likeButton.setImage(UIImage(named: "like_button_off"), for: .normal)
+        likeButton.setImage(UIImage(named: "like_button_on"), for: .selected)
+    }
 }
 
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -21,27 +21,20 @@ final class ImagesListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
-       
     }
     
     private func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
         let imageName = photosName[indexPath.row]
         
-        // Проверяем, можно ли создать UIImage
         guard let image = UIImage(named: imageName) else {
             return
         }
         
         cell.cellImage.image = image
         
-        // Устанавливаем текущую дату
         let currentDate = Date()
         cell.dateLabel.text = dateFormatter.string(from: currentDate)
-        
-        // Устанавливаем состояние кнопки лайка
         cell.likeButton.isSelected = indexPath.row % 2 == 0
     }
     
@@ -52,15 +45,14 @@ extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return photosName.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
-
+        
         guard let imageListCell = cell as? ImagesListCell else {
-            print("Ошибка: Ячейка не является типом ImagesListCell")
             return UITableViewCell()
         }
-
+        
         configCell(for: imageListCell, with: indexPath)
         return imageListCell
     }
@@ -69,28 +61,21 @@ extension ImagesListViewController: UITableViewDataSource {
 // MARK: - UITableViewDelegate
 
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // TODO: - Добавить логику при нажатии на ячейку
+    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-
-            // Получаем имя изображения
-            let imageName = photosName[indexPath.row]
-            
-            // Получаем изображение из ресурсов
-            guard let image = UIImage(named: imageName) else {
-                // Возвращаем стандартную высоту, если изображения нет
-                return 200
-            }
-            
-            // Вычисляем ширину изображения
-            let imageWidth = tableView.bounds.width - 32 // отступы по 16 слева и справа
-            
-            // Вычисляем высоту, сохраняя соотношение сторон
-            let imageHeight = image.size.height / image.size.width * imageWidth
-            
-            // Возвращаем высоту с учетом отступов сверху и снизу
-            return imageHeight + 16 + 8 // 16 сверху, 8 снизу
+        
+        let imageName = photosName[indexPath.row]
+        guard let image = UIImage(named: imageName) else {
+            // Возвращаем стандартную высоту, если изображения нет
+            return 200
         }
-
-    
+   
+        let imageWidth = tableView.bounds.width - 32 // отступы по 16 слева и справа
+        let imageHeight = image.size.height / image.size.width * imageWidth
+        
+        return imageHeight + 16 + 8 // 16 сверху, 8 снизу
+    }
 }
