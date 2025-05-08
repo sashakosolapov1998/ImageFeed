d927054455bac8a587a920df55e257c59c9c28fd
Александр Косолапов <sashakosolapov1998@yandex.ru>
2025-05-08 19:48:48 +0300

    Добавлены недостающие файлы через терминал

---
+++ Resources/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  ImageFeed
+//
+//  Created by Александр Косолапов on 08.04.2025
+
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "YPi2RHVHvJZk9SQz9kiJEr7ZKkzq8rGHll9jYU4uoaw"
+    static let secretKey = "_Unu58K-mOhrjJ2GPACiL5jZJvdknnoNRf8RpTntFOs"
+    static let redirectURI = "https://example.com/callback"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL: URL = {
+        guard let url = URL(string: "https://api.unsplash.com") else {
+            preconditionFailure("Ошибка: неверный base URL")
+        }
+        return url
+    }()
+}
